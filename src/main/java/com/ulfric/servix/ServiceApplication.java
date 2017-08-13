aa0e0ac4249ec63aed4f1a3c0602d2a70f39676f
Adam Harrison <ad@mharrison.us>
2017-08-13 13:11:54 -0400

    @Inject services

--- src/main/java/com/ulfric/servix/ServiceApplication.java
+++ src/main/java/com/ulfric/servix/ServiceApplication.java
@@ -1,12 +1,18 @@
 package com.ulfric.servix;
 
+import com.ulfric.dragoon.ObjectFactory;
 import com.ulfric.dragoon.application.Application;
+import com.ulfric.dragoon.extension.inject.Inject;
+import com.ulfric.dragoon.reflect.Classes;
 
 import java.util.Objects;
 
 public class ServiceApplication extends Application {
 
 	private final Service<?> service;
+
+	@Inject
+	private ObjectFactory factory;
 
 	public ServiceApplication(Service<?> service) {
 		Objects.requireNonNull(service, "service");
@@ -19,9 +25,11 @@ public class ServiceApplication extends Application {
 
 	private void register() {
 		Services.register(service);
+		factory.bind(Classes.getNonDynamic(service.getService())).toValue(service);
+		factory.bind(Classes.getNonDynamic(service.getClass())).toValue(service);
 	}
 
-	private void unregister() {
+	private void unregister() { // TODO fix leaking services
 		Services.unregister(service);
 	}
 
--- src/main/java/com/ulfric/servix/ServiceFeature.java
+++ src/main/java/com/ulfric/servix/ServiceFeature.java
@@ -1,13 +1,18 @@
 package com.ulfric.servix;
 
+import com.ulfric.dragoon.ObjectFactory;
 import com.ulfric.dragoon.application.Application;
 import com.ulfric.dragoon.application.Feature;
+import com.ulfric.dragoon.extension.inject.Inject;
 
 public class ServiceFeature extends Feature {
 
+	@Inject
+	private ObjectFactory factory;
+
 	@Override
 	public Application apply(Object service) {
-		return service instanceof Service ? new ServiceApplication((Service<?>) service) : null;
+		return service instanceof Service ? factory.request(ServiceApplication.class, service) : null;
 	}
 
 }
